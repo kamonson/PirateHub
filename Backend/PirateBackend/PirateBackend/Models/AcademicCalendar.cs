1f47e84606a7350cf58fc82042647a8345cf9ecb
Kyle <kyle.b.amonson@hotmail.com>
2016-04-30 16:36:32 -0700

    initial commit

---
+++ Backend/PirateBackend/PirateBackend/Models/AcademicCalendar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PirateBackend.Models
+{
+    public class AcademicCalandar
+    {
+        public int AcademicCalandarId { get; set; }
+        public string AcademicCalandarTytle { get; set; }
+        public string AthleticEventDescription { get; set; }
+        public DateTime AthleticEventDateTime { get; set; }
+        public string Self
+        {
+            get
+            {
+                return string.Format(CultureInfo.CurrentCulture,
+               "api/academicCalandar/{0}", this.AcademicCalandarId);
+            }
+            set { }
+        }
+    }
+}
