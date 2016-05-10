0e68b9a7d54a776ab9b6cdb6dac5b85291b3f99e
Kyle <kyle.b.amonson@hotmail.com>
2016-05-10 10:44:47 -0700

    Conflict Merge From Branches Resolved

--- Backend/PirateBackend/PirateBackend/Migrations/Configuration.cs
+++ Backend/PirateBackend/PirateBackend/Migrations/Configuration.cs
@@ -13,43 +13,43 @@ namespace PirateBackend.Migrations
             AutomaticMigrationsEnabled = true;
         }
 
-        protected override void Seed(PirateBackend.Models.PirateBackendContext context)
-        {
-            context.AthleticEvents.AddOrUpdate(p => p.AthleticEventType,
-               new AthleticEvent
-               {
-                   AthleticEventType = "BASKetball",
-                   AthleticEventDescription = "Like the love child of basketball and baseball",
-                   AthleticEventDateTime = DateTime.Now,
-                   AthleticEventLocation = "Milwaukee",
-                   AthleticEventOpponent = "Roswell Aliens"
-               },
-                new AthleticEvent
-                {
-                    AthleticEventType = "Power Walking",
-                    AthleticEventDescription = "It's as gay as it sounds",
-                    AthleticEventDateTime = DateTime.Now,
-                    AthleticEventLocation = "Far From Here",
-                    AthleticEventOpponent = "Self"
-                }
-                );
-            context.AcademicCalandars.AddOrUpdate(p => p.AcademicCalandarTitle,
-                new AcademicCalandar
-                {
-                    AcademicCalandarTitle = "The First Day Of The Rest Of Your Life",
-                    AcademicCalandarDescription = "Another bougas motivational speach",
-                    AcademicCalandarDateTime = DateTime.Now
-                }
-                );
-            context.EventCalandars.AddOrUpdate(p => p.EventCalandarTitle,
-                new EventCalandar
-                {
-                    EventCalandarTitle = "Yet another booring lecture",
-                    EventCalandarLocation = "Dixon Hall",
-                    EventCalandarDescription = "Core 350 talk about food policy, drone on and on and on and ...",
-                    EventCalandarDateTime = DateTime.Now
-                }
-                );
-        }
+        //protected override void Seed(PirateBackend.Models.PirateBackendContext context)
+        //{
+        //    context.AthleticEvents.AddOrUpdate(p => p.AthleticEventType,
+        //       new AthleticEvent
+        //       {
+        //           AthleticEventType = "BASKetball",
+        //           AthleticEventDescription = "Like the love child of basketball and baseball",
+        //           AthleticEventDateTime = DateTime.Now,
+        //           AthleticEventLocation = "Milwaukee",
+        //           AthleticEventOpponent = "Roswell Aliens"
+        //       },
+        //        new AthleticEvent
+        //        {
+        //            AthleticEventType = "Power Walking",
+        //            AthleticEventDescription = "It's as gay as it sounds",
+        //            AthleticEventDateTime = DateTime.Now,
+        //            AthleticEventLocation = "Far From Here",
+        //            AthleticEventOpponent = "Self"
+        //        }
+        //        );
+        //    context.AcademicCalandars.AddOrUpdate(p => p.AcademicCalandarTitle,
+        //        new AcademicCalandar
+        //        {
+        //            AcademicCalandarTitle = "The First Day Of The Rest Of Your Life",
+        //            AcademicCalandarDescription = "Another bougas motivational speach",
+        //            AcademicCalandarDateTime = DateTime.Now
+        //        }
+        //        );
+        //    context.EventCalandars.AddOrUpdate(p => p.EventCalandarTitle,
+        //        new EventCalandar
+        //        {
+        //            EventCalandarTitle = "Yet another booring lecture",
+        //            EventCalandarLocation = "Dixon Hall",
+        //            EventCalandarDescription = "Core 350 talk about food policy, drone on and on and on and ...",
+        //            EventCalandarDateTime = DateTime.Now
+        //        }
+        //        );
+        //}
     }
 }
